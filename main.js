1107ba1815062ec02c11b16450548b5c6199e228
Hridayy <73392185+Hridayy@users.noreply.github.com>
2022-05-19 13:14:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+function preload(){
+    song=loadSound("Tujhe Kitna Chahne Lage-Kabir Singh.mp3")
+    song=loadSound("music.mp3")
+}
 function setup(){
-    video = createCapture(VIDEO)
-    video.size(560,500);
+    canvas=createCanvas(560,500);
+    canvas.center();
+    video=createCapture(VIDEO)
+    video.hide();
+}
+function draw(){
+    image(video,0,50,400,400)
+}
+function play(){
+    song.play();
+    song.setVolume(0.1);
 }
