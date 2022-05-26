f83af312ab31b8ce9b7edebda5129ad9a2d51335
Hridayy <73392185+Hridayy@users.noreply.github.com>
2022-05-26 16:12:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWirstX = 0;
+leftWirstY = 0;
+rightWirstY = 0;
+rightWirstX = 0;
+scoreLeftWrist = 0;
 function preload(){
     song=loadSound("Tujhe Kitna Chahne Lage-Kabir Singh.mp3")
     song=loadSound("music.mp3")
@@ -7,11 +12,47 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO)
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("poses", gotposes)
+
 }
 function draw(){
     image(video,0,50,400,400)
 }
 function play(){
     song.play();
-    song.setVolume(0.1);
+    song.setVolume(1);
+    song.rate(1)
 }
+function modelLoaded() {
+    console.log("PoseNet is Intilized")
+}
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log("results")
+        scoreLeftWrist = results[0].pose.keypoints[9].score
+        console.log("scoreLeftWrist = " + scoreLeftWrist)
+        leftWirstX = results[0].pose.leftWrist.x;
+        leftWirstY = results[0].pose.leftWrist.y;
+        console.log("leftWirstX = " + leftWirstX + "leftWristY" + leftWirstY)
+        rightWirstX = results[0].pose.rightWrist.x;
+        rightWirstY = results[0].pose.rightWrist.y;
+        console.log("rightWirstX = " + rightWirstX + "rightWristY" + rightWirstY)
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 400, 400)
+    fill("red")
+    stroke("red")
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWirstX, leftWirstY, 20)
+        num = Number(leftWirstY)
+        remove_decimals = floor(num)
+        volume = remove_decimals / 500
+        document.getElementById("volume").innerHTML = "volume = " + volume;
+        song.setVolume(volume)
+        console.log("volume = " + volume);
+    }
+}
+
